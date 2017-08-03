6b91a39c983122341aa56b75a94833468049738e
Dominic Brown <yeahdom@gmail.com>
2017-08-02 19:02:05 -0700

    add tile spanw safty

--- board.rb
+++ board.rb
@@ -3,6 +3,7 @@ class Board
 
   def initialize()
     @tile_arr = []
+    @filled_tiles = 0
 
     4.times do |r|
       rowArr = []
@@ -15,26 +16,40 @@ class Board
 
   end # END initialize
 
-  def spawn_two_tiles
-    2.times do |i|
-      randRow = Gosu.random(0,4).floor
-      randCol = Gosu.random(0,4).floor
-
-          randColor = (Gosu.random(0,99999999) + 0x77000000).round
-          tmpTile = Tile.new(randCol*200,randRow*200,2,randColor)
-          @tile_arr[randRow][randCol] = tmpTile
-
+  def spawn_tile
+    randRow = Gosu.random(0,4).floor
+    randCol = Gosu.random(0,4).floor
+    if ((@tile_arr[randRow][randCol].val == 0) && (@filled_tiles < 16))
+      randColor = (Gosu.random(0,99999999) + 0x77000000).round
+      tmpTile = Tile.new(randCol*200,randRow*200,2,randColor)
+      @tile_arr[randRow][randCol] = tmpTile
+      @filled_tiles += 1
+    elsif (@filled_tiles == 16)
+      1
+    else
+      spawn_tile # try again
     end
   end
 
-  def draw
+  def tiles_left
 
+  end
+  def tiles_right
+
+  end
+  def tiles_up
+
+  end
+  def tiles_down
+
+  end
+
+  def draw
     @tile_arr.each_with_index do |row, i|
       row.each_with_index do |tile, j|
         tile.draw
       end
     end
-
   end #end DRAW
 
 end
--- game.rb
+++ game.rb
@@ -16,7 +16,7 @@ class MyWindow < Gosu::Window
     super(WINDOW_WIDTH, WINDOW_HEIGHT, :fullscreen => false)
     self.caption = "Tile sum game" # the caption method must come after the window creation "super()"
     @myBoard = Board.new
-    @myBoard.spawn_two_tiles
+    @myBoard.spawn_tile
     @tiles_moving = false
     @tiles_status_txt = Gosu::Image.from_text( "false", 40 )
     @move_timer = nil
@@ -47,15 +47,19 @@ class MyWindow < Gosu::Window
     if button == Gosu::KbEscape
        self.close!
     elsif button == Gosu::KbSpace
-      @myBoard.spawn_two_tiles
+      @myBoard.spawn_tile
     elsif button == Gosu::KB_LEFT
       trigger_move
+      @myBoard.tiles_left
     elsif button == Gosu::KB_RIGHT
       trigger_move
+      @myBoard.tiles_right
     elsif button == Gosu::KB_UP
       trigger_move
+      @myBoard.tiles_up
     elsif button == Gosu::KB_DOWN
       trigger_move
+      @myBoard.tiles_down
     else
       super
     end
